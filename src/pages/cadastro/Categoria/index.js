069d599820eedc83d8e69b36fe5b076c3c156dde
Robson M. Gomes <robson.mesquita56@gmail.com>
2020-08-01 20:00:58 -0300

    Update Category Registration

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,30 +2,80 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../PageDefault';
+import Button from '../../../components/Button';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+  const defaultCategory = {
+    name: 'Vídeos',
+    description: 'Os melhores vídeos dos melhores canais',
+    color: '#ffffff'
+  };
+  const [categories, setCategories] = useState([defaultCategory]);
+  const [category, setCategory] = useState(defaultCategory);
+
+  function handleChange(e) {
+    setCategory({
+      ...category,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setCategories([
+      ...categories,
+      category
+    ]);
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: <span style={{ color: category.color }}>{category.name}</span></h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
+        {/*STATE*/}
 
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-          />
-        </label>
+        <FormField
+          label='Nome da Categoria:'
+          name='name'
+          type='text'
+          value={category.name}
+          onChange={handleChange}
+        />
 
-        <button>
+        <FormField
+          label='Descrição:'
+          name='description'
+          type="text-area"
+          value={category.description}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label='Cor:'
+          name="color"
+          type="color"
+          value={category.color}
+          onChange={handleChange}
+        />
+
+        <button onClick={handleSubmit}>
           Cadastrar
         </button>
       </form>
 
+      <ul>
+        {categories.map((category, index) => {
+          return (
+            <li key={`${category.name}${index}`} style={{ color: category.color }}>{category.name}</li>
+          );
+        })}
+      </ul>
 
-      <Link to="/">
+      <Button as={Link} to="/">
         Ir para home
-      </Link>
+      </Button>
     </PageDefault>
   )
 }
--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PageDefault from '../../PageDefault';
 import { Link } from 'react-router-dom';
+import Button from '../../../components/Button';
 
 function CadastroVideo() {
     return (
         <PageDefault>
             <h1>Cadastro de Vídeo</h1>
-            <Link to="/cadastro/categoria">Cadastrar</Link>
+            <Button as={Link} to="/cadastro/categoria">Cadastrar</Button>
         </PageDefault>
     );
 }
