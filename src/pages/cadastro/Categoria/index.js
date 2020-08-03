88c2d721feddc80b7995fd5acfadda731e31fb8d
Robson M. Gomes <robson.mesquita56@gmail.com>
2020-08-02 22:05:42 -0300

    Update Server Connection

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,7 +30,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = 'https://robflix-sigma.herokuapp.com/categorias';
       fetch(URL).then(async (serverResponse) => {
         if (serverResponse.ok) {
           const response = await serverResponse.json();
